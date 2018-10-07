29010387c961ebf118b54ffa1705f1e3636fa307
chuong vo <chuong2v@gmail.com>
2018-10-07 13:03:00 +0700

    [storing] - refactoring

--- storing/remember_me.py
+++ storing/remember_me.py
@@ -3,15 +3,28 @@ import os
 # Load the username, if it has been stored previously.
 # Otherwise, prompt for the username and store it.
 dir_path = os.path.dirname(os.path.realpath(__file__))
-filename = dir_path + '/username.json'
 
-try:
-  with open(filename) as f_obj:
-    username = json.load(f_obj)
-except FileNotFoundError:
-  username = input("What is your name? ")
-  with open(filename, 'w') as f_obj:
-    json.dump(username, f_obj)
-    print("We'll remember you when you come back, " + username + "!")
-else:
-  print("Welcome back, " + username + "!")
+def get_stored_username():
+  """Get stored username if available."""
+  filename = dir_path + '/username.json'
+  try:
+    with open(filename) as f_obj:
+      username = json.load(f_obj)
+  except FileNotFoundError:
+    return None
+  else:
+    return username
+
+def greet_user():
+  """Greet the user by name"""
+  username = get_stored_username()
+  if username:
+    print("Welcome back, " + username + "!")
+  else:
+    username = input("What is your name? ")
+    filename = dir_path + '/username.json'
+    with open(filename, 'w') as f_obj:
+      json.dump(username, f_obj)
+      print("We'll remember you when you come back, " + username + "!")
+
+greet_user()
