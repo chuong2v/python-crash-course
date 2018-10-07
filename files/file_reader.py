e948b67ef3f713c18b5ad7d0daf120a124d65f92
chuong vo <chuong2v@gmail.com>
2018-10-07 10:36:34 +0700

    [file] - read

---
+++ files/file_reader.py
@@ -0,0 +1,34 @@
+filename = '/home/chuongvo/Code4Fun/python/files/pi_digits.txt'
+with open(filename) as file_object:
+  contents = file_object.read()
+  print(contents)
+
+with open(filename) as file_object:
+  for line in file_object:
+    print(line) # line.rstrip()
+
+with open(filename) as file_object:
+  lines = file_object.readlines()
+
+for line in lines:
+  print(line)
+
+# Million digits
+
+milion_digits_filename = '/home/chuongvo/Code4Fun/python/files/pi_million_digits.txt'
+with open(milion_digits_filename) as file_object:
+  lines = file_object.readlines()
+pi_string = ''
+for line in lines:
+  pi_string += line.rstrip()
+print(pi_string[:52] + '...')
+print(len(pi_string))
+
+
+# Is your birthday contained in Pi
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+  print("Your birthday appears in the first million digits of pi!")
+else:
+  print("Your birthday does not appear in the first million digits of pi.")
+
